3f6255bc3471a1d1aaae39f92b827932c7d528c9
Madeleine Johnson <mjohnson3038@gmail.com>
2021-11-14 22:24:40 -0500

    configure enzyme and fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Game from './App';
-import shallow from 'enzyme'; // by Airbnb
+import { shallow, mount } from 'enzyme'; // by Airbnb, curly braces around shallow because it is not a default export in enzyme
 
 
-// TODO - why is this test failing?
-describe('directly invoking getLettersForDisplay from component', () => {
-  test('getLettersForDisplay', () => {
+describe('getLettersForDisplay', () => {
+  test('selects correct letters', () => {
     // https://bambielli.com/til/2018-03-04-directly-test-react-component-methods/
   
     const wrapper = shallow(<Game/>);
@@ -16,8 +15,8 @@ describe('directly invoking getLettersForDisplay from component', () => {
       [1,2,3,2,3,0,4,5,2,5,3,4,5,3,0,2]
     )
   
-    expect(lettersForDisplay).toBe(
-      ['F', 'S', 'N', 'O', 'E', 'E', 'O,', 'N', 'O', 'R', 'D', 'I', 'R', 'C']
+    expect(lettersForDisplay).toStrictEqual(
+      ['F', 'S', 'O', 'O', 'E', 'E', 'O', 'E', 'M', 'N', 'O', 'R', 'D', 'I', 'R', 'C']
     );
   });
 })
